97114de4e4f4199a048844ebdaa90fb782bc6ec3
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-12-02 17:12:40 +0300

    did dijkstra algorithm

---
+++ DijkstraAlgorithm/Graph.cs
@@ -0,0 +1,117 @@
+﻿namespace DijkstraAlgorithm
+{
+    class GraphEdge
+    {
+        public GraphEdge(GraphVertex conntected, int edgeWeight)
+        {
+            Conntected = conntected;
+            EdgeWeight = edgeWeight;
+        }
+
+        public GraphVertex Conntected { get; }
+
+        public int EdgeWeight { get; }
+    }
+
+    class GraphVertex
+    {
+        public GraphVertex(string vertexName)
+        {
+            Name = vertexName;
+            Edges = [];
+        }
+
+        public string Name { get; }
+
+        public List<GraphEdge> Edges { get; }
+
+        public void AddEdge(GraphEdge edge)
+        {
+            Edges.Add(edge);
+        }
+
+        public void AddEdge(GraphVertex vertex, int edgeWeight)
+        {
+            AddEdge(new GraphEdge(vertex, edgeWeight));
+        }
+    }
+
+    class Graph
+    {
+        public List<GraphVertex> Vertices { get; }
+
+        public Graph()
+        {
+            Vertices = [];
+        }
+
+        public void AddVertex(string vertexName)
+        {
+            Vertices.Add(new GraphVertex(vertexName));
+        }
+
+        public GraphVertex FindVertex(string vertexName)
+        {
+            foreach (GraphVertex vertex in Vertices)
+            {
+                if (vertex.Name == vertexName) return vertex;
+            }
+
+            return null;
+        }
+
+        public void AddEdge(string startVertex, string targetVertex, int weight)
+        {
+            var firstVertex = FindVertex(startVertex);
+            var secondVertex = FindVertex(targetVertex);
+
+            if (firstVertex != null && secondVertex != null)
+            {
+                firstVertex.AddEdge(secondVertex, weight);
+                secondVertex.AddEdge(firstVertex, weight);
+            }
+        }
+    }
+
+    class GraphVertexInfo
+    {
+        private bool _unvisited;
+        private int _edgesWeightSum;
+        private GraphVertex _previousVertex;
+
+        public GraphVertexInfo(GraphVertex vertex)
+        {
+            Vertex = vertex;
+            _unvisited = true;
+            _edgesWeightSum = int.MaxValue;
+        }
+
+        public GraphVertex Vertex { get; }
+
+        public GraphVertex PreviousVertex
+        {
+            get => _previousVertex;
+            set
+            {
+                if (value != Vertex)
+                    _previousVertex = value;
+                else
+                    _previousVertex = null;
+            }
+        }
+
+        public int EdgesWeightSum
+        {
+            get => _edgesWeightSum;
+            set
+            {
+                if(value >= 0)
+                    _edgesWeightSum = value;
+            }
+        }
+
+        public bool Unvisited => _unvisited;
+
+        public void Visit() => _unvisited = false;
+    }
+}
---
+++ DijkstraAlgorithm/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace DijkstraAlgorithm
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Graph graph = new();
+
+            graph.AddVertex("A");
+            graph.AddVertex("B");
+            graph.AddVertex("C");
+            graph.AddVertex("D");
+            graph.AddVertex("E");
+            graph.AddVertex("F");
+
+            graph.AddEdge("A", "B", 18);
+            graph.AddEdge("A", "C", 40);
+            graph.AddEdge("A", "E", 26);
+            graph.AddEdge("B", "E", 65);
+            graph.AddEdge("B", "D", 20);
+            graph.AddEdge("D", "F", 15);
+            graph.AddEdge("E", "F", 5);
+
+            DijkstraAlgorithm dijkstraAlgorithm = new(graph);
+            string path = dijkstraAlgorithm.FindShortestPath("A", "F");
+            Console.WriteLine(path);
+        }
+    }
+
+    internal class DijkstraAlgorithm
+    {
+        private Graph _graph;
+
+        private List<GraphVertexInfo> _info;
+
+        public DijkstraAlgorithm(Graph graph)
+        {
+            _graph = graph;
+        }
+
+        private void Init()
+        {
+            _info = [];
+
+            foreach (var vertex in _graph.Vertices)
+            {
+                _info.Add(new GraphVertexInfo(vertex));
+            }
+        }
+
+        private GraphVertexInfo GetVertexInfo(GraphVertex vertex)
+        {
+            foreach (var info in _info)
+            {
+                if (info.Vertex == vertex)
+                    return info;
+            }
+
+            return null;
+        }
+
+        public GraphVertexInfo FindVertex(bool unvisited = true, bool minSum = true)
+        {
+            int minValue = int.MaxValue;
+            GraphVertexInfo minVertexInfo = null;
+
+            foreach (var info in _info)
+            {
+                if (info.Unvisited && info.EdgesWeightSum < minValue)
+                {
+                    minVertexInfo = info;
+                    minValue = info.EdgesWeightSum;
+                }
+            }
+
+            return minVertexInfo;
+        }
+
+        public string FindShortestPath(string startName, string targetName)
+        {
+            return FindShortestPath(_graph.FindVertex(startName), _graph.FindVertex(targetName));
+        }
+
+        private string FindShortestPath(GraphVertex startVertex, GraphVertex targetVertex)
+        {
+            Init();
+            var startInfo = GetVertexInfo(startVertex);
+            startInfo.EdgesWeightSum = 0;
+
+            GraphVertexInfo current;
+
+            do
+            {
+                current = FindVertex();
+                SetSumToNextVertex(current);
+            }
+            while (current != null);
+
+            return GetPath(startVertex, targetVertex);
+        }
+
+        private void SetSumToNextVertex(GraphVertexInfo info)
+        {
+            if (info == null)
+                return;
+
+            info.Visit();
+
+            foreach (var edge in info.Vertex.Edges)
+            {
+                GraphVertexInfo nextInfo = GetVertexInfo(edge.Conntected);
+                int sum = info.EdgesWeightSum + edge.EdgeWeight;
+
+                if (sum < nextInfo.EdgesWeightSum)
+                {
+                    nextInfo.EdgesWeightSum = sum;
+                    nextInfo.PreviousVertex = info.Vertex;
+                }
+            }
+        }
+
+        private string GetPath(GraphVertex startVertex, GraphVertex targetVertex)
+        {
+            string path = targetVertex.Name;
+
+            while (startVertex != targetVertex)
+            {
+                targetVertex = GetVertexInfo(targetVertex).PreviousVertex;
+                path = targetVertex.Name + "/" + path;
+            }
+
+            return path;
+        }
+    }
+}
